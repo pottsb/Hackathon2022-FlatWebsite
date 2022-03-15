03a23ccde02b13c5b517658b8a8a797b173e012b
Morgan Godden <100573344@unimail.derby.ac.uk>
2022-03-15 14:43:16 +0000

    Stuff

--- includes/header.php
+++ includes/header.php
@@ -1,10 +1,10 @@
 <div class="headerContainer">
             <div class="logoContainer">
                 <div class="logo"></div>
-
             </div>
+
             <div class="linkbarContainer">
-              
+
                 <div class="linkBar">
                   <div class="titleContainer"><h1>Low Carbon</h1></div>
                   
@@ -13,7 +13,7 @@
                           <a class="nav-link active" href="index.php">Home</a>
                         </li>
                         <li class="nav-item">
-                          <a class="nav-link" href="Keyfacts.php">Key Facts</a>
+                          <a class="nav-link" href="keyfacts.php">Key Facts</a>
                         </li>
                         <li class="nav-item">
                           <a class="nav-link" href="#">Link</a>

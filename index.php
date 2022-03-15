afd05a0bedec16f60158ef04b1b37fb8cb266250
Isaac Augustyniak <100513827@unimail.derby.ac.uk>
2022-03-15 19:21:35 +0000

    pogchamp

--- index.php
+++ index.php
@@ -8,19 +8,21 @@
         <div class="contentContainer indexBody">
             <div class="IndexCol1">
                 <div class="centre">
-                  <h2>EARTH  HAS  TOO<br>MUCH  CARBON!</h2>
+                  <h2>EARTH-CHAN NEEDS<br>YOUR HELP!</h2>
                   <p>
                     Earth-chan does not like too much carbon<br>
-                    We must help her<br>
-                    You wanna be cool right?<br>
-                    All the cool kids are doing it<br>
-                    You wanna be cool right?<br>
-                    All the cool kids are doing it<br>
-                    You wanna be cool right?<br>
-                    All the cool kids are doing it<br>
-                    You wanna be cool right?<br>
-                    All the cool kids are doing it<br>
-                    You wanna be cool right?<br>
+                    We must help Earth-chan and her friends!<br>
+                    <br>
+                    What is carbon dioxide?<br>
+                    It is a colourless gas found in the air.<br>
+                    It is an important greeenhouse gas that helps trap heat in our atmosphere.<br>
+                    <br>
+                    Where does it come from?<br>
+                    It comes from natural (breath of animals, volcanoes) and human sources (burning coal and oil).<br>
+                    <br>
+                    Why is it harmful?<br>
+                    Increased carbon dioxide in the atmosphere, creates a layer that traps the heat energy from the sun.<br>
+                    within the atmosphere, this increases the temperature of our planet and our oceans.
                 </div>
             </div>
             <div class="IndexCol2">

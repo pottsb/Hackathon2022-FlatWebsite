8a0cb2e5881b808d400674f52774c10ab6c9172a
Isaac Augustyniak <100513827@unimail.derby.ac.uk>
2022-03-15 19:28:58 +0000

    pogchamp

--- index.php
+++ index.php
@@ -10,18 +10,18 @@
                 <div class="centre">
                   <h2>EARTH-CHAN NEEDS<br>YOUR HELP!</h2>
                   <p>
-                    Earth-chan does not like too much carbon<br>
+                    Earth-chan does not like too much carbon.<br>
                     We must help Earth-chan and her friends!<br>
                     <br>
-                    What is carbon dioxide?<br>
+                    <strong>What is carbon dioxide?</strong><br>
                     It is a colourless gas found in the air.<br>
                     It is an important greeenhouse gas that helps trap heat in our atmosphere.<br>
                     <br>
-                    Where does it come from?<br>
+                    <strong>Where does it come from?</strong><br>
                     It comes from natural (breath of animals, volcanoes) and human sources (burning coal and oil).<br>
                     <br>
-                    Why is it harmful?<br>
-                    Increased carbon dioxide in the atmosphere, creates a layer that traps the heat energy from the sun.<br>
+                    <strong>Why is it harmful?</strong><br>
+                    Increased carbon dioxide in the atmosphere, creates a layer that traps the heat energy from the sun<br>
                     within the atmosphere, this increases the temperature of our planet and our oceans.
                 </div>
             </div>

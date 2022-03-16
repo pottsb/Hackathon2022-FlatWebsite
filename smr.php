0d4f60abd2b532a31fb7a66a2ddb332672b859d8
pottsb <63508539+pottsb@users.noreply.github.com>
2022-03-16 10:58:33 +0000

    Added SMR content

--- smr.php
+++ smr.php
@@ -9,13 +9,12 @@
         <div class="leftContentParent" style="width: 70%">
             <div class="halfContent" style="flex: 0 0 83.2%">
                     <h2>Rolls Royce SMR</h2>
-                    <h3>Sub title</h3>
-                    <p>SOme text here about out cause</p>
-                    <h4>TEXT</h4>
+                    <h3>What is an SMR?</h3>
                     <ul>
-                        <li>Coffee</li>
-                        <li>Tea</li>
-                        <li>Milk</li>
+                    <li>Rolls Royce are developing their <strong>small modular reactor or SMR</strong>, a modern nuclear power station in <strong>Derby</strong>, to meet the nuclear power needs of the UK and more.</li>
+                    <li>Rolls Royce has <strong>60</strong> years of experience producing small reactors for submarines and believes the SMR will offer <strong>clean, affordable energy for all.</strong></li>
+                    <li>The Rolls Royce SMR team aims to build the <strong>first</strong> power plant by <strong>2030</strong> and <strong>15</strong> more by <strong>2040</strong>, which could create <strong>40,000</strong> jobs in an industry leading field.</li>
+                    <li>People of your age will be part of this project in the future!</li>
                     </ul>
                 </div>
             </div>

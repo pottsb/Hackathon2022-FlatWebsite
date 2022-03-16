a58fb5691cdd315e0e1e5f2edf12bfbe36be3f3c
pottsb <63508539+pottsb@users.noreply.github.com>
2022-03-16 07:18:22 +0000

    Working on flag thingy dingy

--- includes/header.php
+++ includes/header.php
@@ -30,6 +30,10 @@
             <div class="localisationContainer">
                 <div class="localisation">
 
+                <img src="../images/flags/gb.png" width="48" height="36" alt="Great Britan">
+                <img src="../images/flags/de.png" width="48" height="36" alt="Germany">
+                <img src="../images/flags/fr.png" width="48" height="36" alt="France">
+
                 </div>
             </div>
         </div>

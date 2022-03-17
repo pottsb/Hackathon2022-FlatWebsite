9247bde0ab189387b7a2500e2432a3accb1e19bf
Morgan <52884391+MorganGodden@users.noreply.github.com>
2022-03-17 04:05:15 +0000

    Commit

--- game.php
+++ game.php
@@ -5,25 +5,23 @@
   <?php include "includes/head.php" ; ?>
   <?php include "includes/header.php" ; ?>
 
-        <div class="contentContainer backingGame">
-        <div class="starField"></div>
-            <div class="fullContentParent">
-              <div> <div class="earthIdle"></div>
-                <div class="fullContent">
-                  <h2>Join Earth-chan's Adventure!</h2>
-                  <p>
-                    Help <strong>Earth-chan</strong> develop friendships. Every choice is your own!<br>
-                    In this game, you'll meet <strong>Newkey</strong>, <strong>Cole</strong>, and <strong>Sunny</strong>.<br>
-                  </p>
-                    <ul class="nav nav-pills buttonContainer">
-                      <li class="nav-item"><a class="nav-link active" target="_blank" href="game/index.html">PLAY NOW</a></li>
-                    </ul>
-                </div>
-            </div> 
-            
+  <div class="contentContainer backingGame">
+  <div class="starField"></div>
+      <div class="fullContentParent">
+        <div> <div class="earthIdle"></div>
+          <div class="fullContent">
+            <h2>Join Earth-chan's Adventure!</h2>
+            <p>
+              Help <strong>Earth-chan</strong> develop friendships. Every choice is your own!<br>
+              In this game, you'll meet <strong>Nukey</strong>, <strong>Cole</strong>, and <strong>Sunny</strong>.<br>
+            </p>
+            <ul class="nav nav-pills buttonContainer">
+              <li class="nav-item"><a class="nav-link active" target="_blank" href="game/index.html">PLAY NOW</a></li>
+            </ul>
           </div>
-          
-        </div>
+      </div> 
+    </div>
+  </div>
 </div>
 
 

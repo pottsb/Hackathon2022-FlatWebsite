a2d15b049ac7ce258e3e2fc3da404d8f3ab46034
pottsb <63508539+pottsb@users.noreply.github.com>
2022-03-22 10:56:28 +0000

    Fixed title

--- includes/head.php
+++ includes/head.php
@@ -2,15 +2,12 @@
 <html lang="en">
 
 <head>
-<meta charset="utf-8" />
+    <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="title" content="The Fission Project">
     <meta name="description" content="The Fission project educates about the dangers of carbon dioxide through fun facts and an interactive game. Join Earth-chan on her quest to save the world!" />
     <meta name="author" content="Team Fission" />
     <meta name="keywords" content="The Fission Project, Nuclear, Rolls-Royce, SMR, Energy, Power, Green">
-
-
-
 
     <meta property="og:url" content="https://hackathon.bfrd.uk/">
     <meta property="og:type" content="website">
@@ -18,14 +15,13 @@
     <meta property="og:description" content="The Fission project educates about the dangers of carbon dioxide through fun facts and an interactive game. Join Earth-chan on her quest to save the world!">
     <meta property="og:image" content="https://hackathon.bfrd.uk/images/earthchan_neutral.png">
 
-
     <meta property="twitter:card" content="summary_large_image">
     <meta property="twitter:url" content="https://hackathon.bfrd.uk/">
     <meta property="twitter:title" content="The Fission Project">
     <meta property="twitter:description" content="The Fission project educates about the dangers of carbon dioxide through fun facts and an interactive game. Join Earth-chan on her quest to save the world!">
     <meta property="twitter:image" content="https://hackathon.bfrd.uk/images/earthchan_neutral.png">
 
-    <title>Team Fission - Low Carbon</title>
+    <title>The Fission Project</title>
     <link rel="icon" type="image/x-icon" href="images/favicon.ico">
 
     <link href="scripts/bootstrap/bootstrap.min.css" rel="stylesheet" />
